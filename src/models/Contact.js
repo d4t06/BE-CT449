80fe2792c889fefaf8d7e05479273ab54ca02280
d4t06 <huudat01234560@gmail.com>
2023-02-23 17:05:59 +0700

    update

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
   name: { type: String },
-  favorite: { type : Boolean, default: null},
+  email: { type: String},
+  phone: {type: String},
+  address: {type: String},
+  favorite: { type : Boolean},
 });
 
 module.exports = mongoose.model('Contact', ContactSchema);
